6eeb8096418dda7f3d83f23267ead58651389a2d
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2012-08-09 19:33:22 -0300

    Create *clean_project* shell-script

---
+++ clean_project.sh
@@ -0,0 +1,8 @@
+# JavaScript files
+rm -Rf Resources/*.js
+rm -Rf Resources/lib/*.js
+rm -Rf Resources/ui/*.js
+rm -Rf Resources/android/ui/*.js
+rm -Rf Resources/iphone/ui/*.js
+rm -Rf Resources/android/lib/*.js
+rm -Rf Resources/iphone/lib/*.js
